c71345e30f6f01e5bf2296151b2e269eeeaf1477
a.salagaev <turbo.cunnos@gmail.com>
2022-05-15 11:51:24 +0300

    working release

--- hedgehog/config/prod.exs
+++ hedgehog/config/prod.exs
@@ -5,3 +5,38 @@ config :naive,
 
 config :streamer,
   binance_client: Binance
+
+
+config :data_warehouse, DataWarehouse.Repo,
+  database: "data_warehouse",
+  username: "postgres",
+  password: "hedgehogSecretPassword",
+  hostname: "db"
+
+
+config :streamer, Streamer.Repo,
+  database: "streamer",
+  username: "postgres",
+  password: "hedgehogSecretPassword",
+  hostname: "db"
+
+config :naive,
+  leader: Naive.Leader,
+  trading: %{
+    defaults: %{
+      chunks: 5,
+      budget: 1000,
+      buy_down_interval: "0.0001",
+      profit_interval: "-0.0012",
+      rebuy_interval: "0.001"
+    }
+  }
+
+config :naive, Naive.Repo,
+  database: "naive",
+  username: "postgres",
+  password: "hedgehogSecretPassword",
+  hostname: "db"
+
+config :binance_mock,
+  use_cached_exchange_info: false
